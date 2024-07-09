b7fb56c400393fd4310337fe37a2f909f3f87871
Ricardo S. Ferreira <ricardo.silva_ferreira@hotmail.com>
2024-07-09 14:50:53 -0300

    Controle de fluxo Dio

---
+++ src/desafiocontrolefluxo_dio/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+package desafiocontrolefluxo_dio;
+
+
+public class ParametrosInvalidosException extends Exception {
+	
+		public ParametrosInvalidosException(String mensagem) {
+	        super(mensagem);
+	    }
+	}
+
